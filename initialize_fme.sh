084f43d79e1a9fd68b8f336d32b64295a0804316
João F. Ferreira <joao@joaoff.com>
2021-08-21 14:08:37 +0100

    Update initialize script

--- initialize_fme.sh
+++ initialize_fme.sh
@@ -1,3 +1,4 @@
+git submodule add https://github.com/fme-teaching/fme-teaching.github.io.git public
 git submodule update --init --recursive
 git submodule foreach git checkout master
 git submodule foreach git pull origin master 
