7a63f69aea7246e4c30941f0caeb31a30850b04f
Joao F. Ferreira <joao@joaoff.com>
2022-10-25 12:22:58 +0100

    Remove unnecessary scripts

--- initialize_fme.sh
+++
@@ -1,8 +0,0 @@
-git submodule add https://github.com/fme-teaching/fme-teaching.github.io.git public
-git submodule update --init --recursive
-git submodule foreach git checkout master
-git submodule foreach git pull origin master 
-#cd themes/academic/
-#./add_remote.sh 
-#cd ../..
-#./add_remote.sh
